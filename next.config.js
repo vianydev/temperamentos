97f7cad999528e274908dfa20fd9fb969c697bcb
Vianey Marcelo <vianymoz@gmail.com>
2023-06-23 12:43:07 -0600

    setup gh-pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  basePath: '/temperamentos',
+  assetPrefix: '/temperamentos/',
+}
 
 module.exports = nextConfig
