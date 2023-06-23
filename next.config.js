5c81a6cd6fc5ce9ce006f9c726854db7148e7166
Vianey Marcelo <vianymoz@gmail.com>
2023-06-23 15:19:38 -0600

    deploy updates

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
+// const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
-  // basePath: '/temperamentos',
-  // assetPrefix: '/temperamentos/',
-  assetPrefix: isProd ? '/temperamentos' : '',
-  assetPrefix: isProd ? '/temperamentos/' : undefined,
+  basePath: '/temperamentos',
+  assetPrefix: '/temperamentos/',
+  // basePath: isProd ? '/temperamentos' : '',
+  // assetPrefix: isProd ? '/temperamentos/' : undefined,
   output: 'export',
 }
 
