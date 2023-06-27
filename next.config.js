235ed65d87ec89281deda47b65bc4ffb2e4df1a0
Vianey Marcelo <vianymoz@gmail.com>
2023-06-27 12:56:37 -0600

    fix type style issees

--- next.config.js
+++ next.config.js
@@ -3,9 +3,10 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   // basePath: isProd ? '/temperamentos' : '',
-  // assetPrefix: isProd ? '/temperamentos/' : undefined,
-  basePath: '/temperamentos',
-  assetPrefix: '/temperamentos/',
+  // assetPrefix: isProd ? '/temperamentos/' : '',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH ? `/${process.env.NEXT_PUBLIC_BASE_PATH}` : '',
+  reactStrictMode: true,
   output: 'export',
 }
 
